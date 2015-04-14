b9834e62359250107eebfe0a9e2d95f40ed02d2c
Zogg <zoggified@gmail.com>
2015-04-14 20:09:54 +0300

    Automatic package finding in the build directory.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -19,7 +19,7 @@ setup(
     license = "GPLv3",
     keywords = "elegant microservice flow",
     url = "https://github.com/Zogg/docker-alchemy",
-    packages=['libs'],
+    packages=find_packages(),
     long_description=read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
