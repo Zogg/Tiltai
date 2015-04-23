24d36abd68e46691f80cffc4a41d2ed172b78d05
Zogg <zoggified@gmail.com>
2015-04-23 19:24:51 +0300

    Too many spaces in logging format

--- tiltai/utils.py
+++ tiltai/utils.py
@@ -2,6 +2,4 @@
 # Logging
 #
 
-tiltai_logs_format = '[{record.time:%Y-%m-%d %H:%M:%S.%f}] {record.level_name}: \
-                      {record.channel} - {record.filename}:{record.lineno} - \
-                      {record.func_name}: {record.message}'
+tiltai_logs_format = '[{record.time:%Y-%m-%d %H:%M:%S.%f}] {record.level_name}: {record.channel} - {record.filename}:{record.lineno} - {record.func_name}: {record.message}'
