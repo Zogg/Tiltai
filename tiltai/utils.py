2693ea943770f7c4929075ac6cd8a133244912d6
Zogg <zoggified@gmail.com>
2015-04-23 19:27:37 +0300

    Debug logging format

--- tiltai/utils.py
+++ tiltai/utils.py
@@ -2,4 +2,6 @@
 # Logging
 #
 
-tiltai_logs_format = '[{record.time:%Y-%m-%d %H:%M:%S.%f}] {record.level_name}: {record.channel} - {record.filename}:{record.lineno} - {record.func_name}: {record.message}'
+tiltai_logs_format = '[{record.time:%Y-%m-%d %H:%M:%S.%f}] {record.level_name}: {record.channel} - {record.func_name}: {record.message}'
+
+tiltai_logs_format_debug = '[{record.time:%Y-%m-%d %H:%M:%S.%f}] {record.level_name}: {record.channel} - {record.filename}:{record.lineno} - {record.func_name}: {record.message}'
